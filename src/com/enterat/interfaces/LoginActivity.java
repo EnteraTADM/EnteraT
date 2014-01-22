b8ff097f9ed9f2d88c7badd2bd4a24aacb2ad2c7
Christian <ferfelx@gmail.com>
2014-01-23 00:39:30 +0100

    No se mantiene la sesión del profesor

--- src/com/enterat/interfaces/LoginActivity.java
+++ src/com/enterat/interfaces/LoginActivity.java
@@ -267,6 +267,7 @@ public class LoginActivity extends Activity {
 
 	
 	public void obtenerDatosImparteProfesorPorIdUsuario(Usuario us){
+		this.usuario = us;
 		//Guardar las asignaturas que imparte dicho profesor...
 		Imparte imparte = new Imparte();
 		asignaturas = imparte.queImparteProfesorPorIdUsuario( us.getIdUsuario() );
