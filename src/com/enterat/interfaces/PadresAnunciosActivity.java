45eb61083887c621892e8b29202213075626956d
chferfe <ferfelx@gmail.com>
2014-01-22 19:08:28 +0100

    Anuncios del alumno en background

--- src/com/enterat/interfaces/PadresAnunciosActivity.java
+++ src/com/enterat/interfaces/PadresAnunciosActivity.java
@@ -2,11 +2,21 @@ package com.enterat.interfaces;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.enterat.R;
 import com.enterat.bda.Anuncio;
+import com.enterat.services.IConexion;
+import com.enterat.services.WSConection;
+import com.enterat.util.Constantes;
 import com.enterat.util.MyListAdapter;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -26,42 +36,10 @@ public class PadresAnunciosActivity extends Activity {
 		
 		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.activity_padres_tasks);	
-		
-		//
-		HashMap<String, Object> item;
-		ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
-		
-		Anuncio anuncio = new Anuncio();		
-		SharedPreferences preferences = getSharedPreferences("LogIn",Context.MODE_PRIVATE);				
-		ArrayList<Anuncio> listaAnuncios = anuncio.obtenerAnuncios( preferences.getInt("cursoAlumno", 0) );
-		
-		for(int i = 0; i < listaAnuncios.size(); i++){
-			
-			anuncio = listaAnuncios.get(i);
-			
-			item = new HashMap<String, Object>();		
-			item.put("Icon", android.R.drawable.ic_menu_my_calendar);
-			item.put("Title", anuncio.getAsignatura().getAsignatura());
-			item.put("Date", anuncio.getFecha());
-			item.put("Description", anuncio.getContenido());
-			data.add(item);	
-		}
-		
-		MyListAdapter adapter = new MyListAdapter(this, R.layout.list_view_item, data);
-		ListView list = (ListView) findViewById(R.id.lvList);
-		list.setAdapter(adapter);		
-		
-		OnItemClickListener listener = new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				// TODO Auto-generated method stub
-				//Toast.makeText(arg0.getContext(), getResources().getString(R.string.row) + ": " + arg3, Toast.LENGTH_SHORT).show();
-			}
-		};
-		list.setOnItemClickListener(listener);
-		
-		registerForContextMenu(list);		
+		ObtenerAnunciosTask anunciosTask = new ObtenerAnunciosTask();
+		anunciosTask.execute();
+					
 	}
 
 	@Override
@@ -91,6 +69,112 @@ public class PadresAnunciosActivity extends Activity {
 	
 		Toast.makeText(getApplicationContext(),"Ordenado correctamente", Toast.LENGTH_LONG).show();
 		
+	}
+	
+	private class ObtenerAnunciosTask extends AsyncTask<String,Integer,Void>
+	{		
+		@Override
+        protected Void doInBackground(String... params) 
+		{									
+			obtenerAnunciosPorIdAlumno();		
+			
+			return null;
+        }		
+	}
+	
+	private void obtenerAnunciosPorIdAlumno(){
+		SharedPreferences preferences = getSharedPreferences("LogIn",Context.MODE_PRIVATE);		
+		int idCurso =  preferences.getInt("cursoAlumno", 0);
+		
+		String sql1 = "SELECT a.asignatura, t.fecha, t.concepto FROM CURSO c, ASIGNATURA a, ANUNCIO t";
+		String sql2 = " WHERE c.id_curso = " + idCurso + " and c.id_curso = a.id_curso and a.id_asignatura = t.id_asignatura";
+
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("sqlquery1", sql1));
+		pairs.add(new BasicNameValuePair("sqlquery2", sql2));
+
+		new WSConection(pairs, "service.executeSQL.php", Constantes.SQL_CONSULTAR, Constantes.SERV_IMPARTE, new IConexion() {
+			
+			@Override
+			public void getJsonFromWS(JSONObject json) {
+				// TODO Auto-generated method stub
+				//Si se ha obtenido...
+				if(json != null)
+				{
+					rellenarDatos(json);
+				}
+				
+			}
+		});				
+	}
+	
+	
+	private void rellenarDatos(JSONObject json){
+		
+		HashMap<String, Object> item;
+		ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+		ArrayList<Anuncio> listaAnuncios = new ArrayList<Anuncio>();
+		
+		Anuncio anuncio = new Anuncio();		
+	
+		boolean continuar = true;
+		int i = 0;
+
+		while (continuar){
+
+			String txt1 = "subject"+i;
+			String txt2 = "contenido"+i;
+			String txt3 = "date"+i;
+			if (json.has(txt1))
+			{
+				anuncio = new Anuncio();
+
+				try {
+					anuncio.getAsignatura().setAsignatura( json.getString( txt1 ) );
+				
+					if (json.has( txt2 )) { anuncio.setContenido( json.getString( txt2 ) ); }
+					if (json.has( txt3 )) { anuncio.setFecha( json.getString( txt3 ) ); }
+					
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+
+				listaAnuncios.add( anuncio );
+			}
+			else{
+				continuar = false;
+			}
+			i++;
+		}
+		
+		for(int j = 0; j < listaAnuncios.size(); j++){
+			
+			anuncio = listaAnuncios.get(j);
+			
+			item = new HashMap<String, Object>();		
+			item.put("Icon", android.R.drawable.ic_menu_my_calendar);
+			item.put("Title", anuncio.getAsignatura().getAsignatura());
+			item.put("Date", anuncio.getFecha());
+			item.put("Description", anuncio.getContenido());
+			data.add(item);	
+		}
+		
+		MyListAdapter adapter = new MyListAdapter(this, R.layout.list_view_item, data);
+		ListView list = (ListView) findViewById(R.id.lvList);
+		list.setAdapter(adapter);		
+		
+		OnItemClickListener listener = new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				//Toast.makeText(arg0.getContext(), getResources().getString(R.string.row) + ": " + arg3, Toast.LENGTH_SHORT).show();
+			}
+		};
+		list.setOnItemClickListener(listener);
+		
+		registerForContextMenu(list);		
 	}
 	
 }
